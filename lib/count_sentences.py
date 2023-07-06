bc46bb28d117ae0f911d34e0a1115ecace78e6e6
Wesley Peters <wesleypeters@gmail.com>
2023-07-05 20:09:33 -0600

    create class and methods, pass tests

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
+import re
+
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if not isinstance(value, str):
+            print("The value must be a string.")
+        else:
+            self._value = value
+
+    def is_sentence(self):
+        return self._value.endswith('.')
+
+    def is_question(self):
+        return self._value.endswith('?')
+
+    def is_exclamation(self):
+        return self._value.endswith('!')
+
+    def count_sentences(self):
+        sentences = re.split('[.!?]', self._value)
+        return len([s for s in sentences if s.strip() != ''])
+
